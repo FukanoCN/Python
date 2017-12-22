9410ee2781dddc1cb82f6ebc5f7d3fd4dc2b7985
FukanoCN <34767868+FukanoCN@users.noreply.github.com>
2017-12-22 20:14:29 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print('hello world')
+print('i love you')
+temp=input("我要不要坚持，要的话打1")
+guess=int(temp)
+if guess==1:
+    print("这是你这辈子做的最好的选择")
+    print("好好爱她")
+else:
+    print("你是大傻子吧")
+print("珍惜她")
